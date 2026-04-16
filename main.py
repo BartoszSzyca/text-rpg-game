ee238310c5522c110958fb034d41dbdf0b035dd5
Bitrax <Bartosz.Szyca@gmail.com>
2026-04-16 13:41:31 +0200

    "feat: add player and entity positioning to world grid"

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ print(world[0])
 print(world[1])
 print(world[2])
 
+player = "P"
+entity = "E"
+
+world[0][1] = player
+world[2][2] = entity
+
+print(" --- TEST ---")
+print("*" * 50)
+
+print(world[0])
+print(world[1])
+print(world[2])
